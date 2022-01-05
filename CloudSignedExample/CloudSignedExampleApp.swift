d3bb64624ff4e3d32d62197de5f07cc79c848437
alexandre-pod <24512899+alexandre-pod@users.noreply.github.com>
2022-01-05 22:16:04 +0100

    Initial Commit

---
+++ CloudSignedExample/CloudSignedExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudSignedExampleApp.swift
+//  CloudSignedExample
+//
+//  Created by Alexandre Podlewski on 05/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct CloudSignedExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
